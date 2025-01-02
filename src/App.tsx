0a4c12464488329f10b704e7645b68b4c4e4728a
coderayjs <livetalk001@gmail.com>
2025-01-02 22:21:29 +0100

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import { Footer } from './components/layout/Footer'
 import { HomePage } from './pages/HomePage'
 import { ExchangeRates } from './components/exchange/ExchangeRates'
 import { ClientStoriesPage } from './pages/ClientStoriesPage'
+import './index.css'
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col bg-background">
+      <div className="min-h-screen flex flex-col bg-background"> 
         <Navbar />
         <main className="flex-grow">
           <Routes>
